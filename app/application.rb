59f75a0e865b3c359ea32d08ca32706ba244bbb4
Daniel Reina <dannyflatiron@gmail.com>
2020-02-12 19:58:04 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -4,7 +4,7 @@ class Application
     resp = Rack::Response.new
     time = Time.now
 
-    if time >= 12
+    if time.hour >= 12
       resp.write "Good Afternoon!"
     else
       resp.write "Good Morning!"
