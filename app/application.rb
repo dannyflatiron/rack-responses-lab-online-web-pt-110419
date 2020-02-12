6ac3575e4072f9462f94993581ea69fa1623486d
Daniel Reina <dannyflatiron@gmail.com>
2020-02-12 19:52:01 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -4,9 +4,13 @@ class Application
   def call(env)
     resp = Rack::Response.new
     time = Time.now
-    binding.pry
 
-    # resp.finish
+    if time => 1200
+      resp.write "Good Afternoon!"
+    else
+      resp.write "Good Morning!"
+    end
+    resp.finish
   end
 
 end
